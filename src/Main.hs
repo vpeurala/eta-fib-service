f00eee5bce289ee066719a10d7cf4344f2600aa6
Ville Peurala <ville.peurala@gmail.com>
2019-02-08 12:30:25 +0200

    Add a second simple route

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,36 @@
 module Main where
 
-import Servant
-import Servant.Server
-import Data.Aeson
-import Network.Wai.Servlet.Handler.Jetty
+import qualified Servant as S
+import Servant ((:<|>), (:>))
+import qualified Servant.Server as SS
+import qualified Data.Aeson as A
+import Data.Aeson ((.=))
+import qualified Network.Wai.Servlet.Handler.Jetty as J
 
-type API = "fibonacci" :> Capture "n" Int :> Get '[JSON] Value
+type API = "fibonacci" :> S.Capture "n" Int :> S.Get '[S.JSON] A.Value
+      :<|> "backwards" :> S.Capture "s" String :> S.Get '[S.JSON] A.Value
 
 fib :: Int -> Int
 fib 0 = 1
 fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
-api :: Proxy API
-api = Proxy
+doReverse :: String -> String
+doReverse s = reverse s
 
-server :: Application
-server = serve api fibHandler
+api :: S.Proxy API
+api = S.Proxy
+
+server :: SS.Server API
+server = fibHandler S.:<|> revHandler
   where fibHandler n
           | n >= 0
-          = return $ object ["n" .= n, "fib" .= fib n]
-          | otherwise = throwError $ err412 { errBody = "Precondition Failed: n >= 0" }
+          = return $ A.object ["n" .= n, "fib" .= fib n]
+          | otherwise = S.throwError $ SS.err412 { SS.errBody = "Precondition Failed: n >= 0" }
+        revHandler s = return $ A.object ["original" .= s, "backwards" .= (reverse s)]
+
+app :: SS.Application
+app = SS.serve api server
 
 main :: IO ()
-main = run 9000 server
+main = J.run 9000 app
